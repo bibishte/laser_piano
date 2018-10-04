35a9b3fcd213d4ed4f574129de8d2b30fce224b2
bibishte <bibishte@gmail.com>
2018-10-04 12:03:19 +0300

    added code for one laser

---
+++ arduino_laser.ino
@@ -0,0 +1,18 @@
+#define NOTE_1 2
+#define sound 9
+void setup() {
+  pinMode(sound, OUTPUT);
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (analogRead(A0) < 800)
+  {
+     tone(sound, 523, 130);
+     Serial.println( analogRead(A0));
+     delay(80);
+  }
+}
