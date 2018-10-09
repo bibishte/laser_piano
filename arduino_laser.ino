674ee4a1d8914c1a83dd4949113f9404ffef9edc
bibishte <bibishte@gmail.com>
2018-10-09 13:41:35 +0300

    working with processing

--- arduino_laser.ino
+++ arduino_laser.ino
@@ -1,4 +1,8 @@
-#define NOTE_1 2
+#define NOTE_1 A0
+#define NOTE_2 A1
+#define NOTE_3 A2
+#define NOTE_4 A3
+#define NOTE_5 A4
 #define sound 9
 void setup() {
   pinMode(sound, OUTPUT);
@@ -8,18 +12,54 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (analogRead(A0) < 800)
+  //Serial.println(analogRead(A0));
+  
+  if (analogRead(NOTE_1) < 900)
   {
      tone(sound, 523, 130);
      //Serial.println( analogRead(A0));
      Serial.println("a\n");
-     delay(100);
+     delay(1000);
+     return;
   }
-  else
+  if (analogRead(NOTE_2) < 900)
   {
-    Serial.println("n\n");
-    delay(100);
-    
+      tone(8, 587, 130);
+     //Serial.println( analogRead(A0));
+     Serial.println("s\n");
+     delay(1000);
+     return;
   }
+
+  if (analogRead(NOTE_3) < 900)
+  {
+     tone(8, 659, 130);
+     //Serial.println( analogRead(A0));
+     Serial.println("d\n");
+     delay(1000);
+     return;
+  }
+
+  if (analogRead(NOTE_4) < 900)
+  {
+     tone(8, 699, 130);
+     //Serial.println( analogRead(A0));
+     Serial.println("f\n");
+     delay(1000);
+     return;
+  }
+
+  if (analogRead(NOTE_5) < 900)
+  {
+     tone(8, 784, 130);
+     //Serial.println( analogRead(A0));
+     Serial.println("g\n");
+     delay(1000);
+     return;
+  }
+  
+  Serial.println("n\n");
+  delay(100);
+    
+  
 }
